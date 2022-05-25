a4a4a8b1d8080a0c5b24fc6e02df1e193acec604
ReichertA2 <aydee0112@yahoo.com>
2022-05-25 15:34:37 -0500

    added the switch button

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Button from './components/Button';
 import Error from './components/Error';
 import NavBar from './components/NavBar';
 import BookCard from './components/BookCard';
+import Switch from './components/Switch';
 
 function App() {
   return (
       <NavBar>
         <BookCard></BookCard>
+        <Switch></Switch>
         <Button color="success">Click this button</Button>
         <Error style={{backgroundColor:'cornflowerblue'}}>This is an error Message</Error>
       </NavBar>
