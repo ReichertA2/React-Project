adba95b5a3d5b8985faaeb49966e830309101b97
ReichertA2 <aydee0112@yahoo.com>
2022-05-25 23:11:44 -0500

    added the login form

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import Button from './components/Button';
 import NavBar from './components/NavBar';
 import BookCard from './components/BookCard';
 import Switch from './components/Switch';
+import LoginForm from './forms/LoginForm';
 
 function App() {
   return (
       <NavBar>
         <BookCard></BookCard>
         <Switch></Switch>
-        <Button>Login</Button>
+        <Button>Log Out</Button>
         {/* <Error style={{backgroundColor:'cornflowerblue'}}>This is an error Message</Error> */}
+
+        <LoginForm/>
       </NavBar>
   );
 }
