3603539d6a35b97c8d0ddefa995459cefa4a4a02
ReichertA2 <aydee0112@yahoo.com>
2022-06-04 19:47:18 -0500

    the component card has been fixed

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -44,75 +44,46 @@ export default function BookCard() {
     }
     
 
-    // return (
-//         <Card sx={{ maxWidth: 345 }}>
-//             <CardMedia
-//                 component="img"
-//                 height="230"
-//                 image="https://res.cloudinary.com/dla9zwzty/image/upload/v1653508898/wizard_of_oz_gt9dfl.jpg"
-//                 alt="green iguana"
-//             />
-//             <CardContent>
-//                 <Typography gutterBottom variant="h4" component="div">
-//                     The Wonderful Wizard of Oz
-//                 </Typography>
-//                 <Typography gutterBottom variant="h6" component="div">
-//                     L. Frank Baum
-//                 </Typography>
-//                 <Typography variant="body2" color="text.secondary">
-//                     A modern fairy tale with a distinctly American setting, a delightfully levelheaded and assertive heroine, and engaging fantasy characters, the story was enormously popular and became a classic of children’s literature. However, by the late 20th century the 1939 film The Wizard of Oz had become more familiar than the book on which it was based.
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+                component="img"
+                height="230"
+                image={books.img}
+                alt={books.title}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h4" component="div">
+                    {books.title}
+                </Typography>
+                <Typography gutterBottom variant="h6" component="div">
+                    {books.author}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {books.summary}
             
-//                 </Typography>
-//                 <Typography variant="body2" color="text.secondary">
-//                     76 pages
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {books.pages}
             
-//                 </Typography>
+                </Typography>
                 
                 
-//             </CardContent>
-//             <CardActions>
-//                 <Button size="small">Add</Button>
-//                 <Button size="small">Remove</Button>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Add</Button>
+                <Button size="small">Remove</Button>
 
-//                 {/* below is the formGroup code that is here if you want to remove later */}
-//                 <FormGroup>
-//                     <FormControlLabel control={<Switch defaultChecked />} label="Read/Unread" />
-//                 </FormGroup>
+                {/* below is the formGroup code that is here if you want to remove later */}
+                <FormGroup>
+                    <FormControlLabel control={<Switch defaultChecked />} label="Read/Unread" />
+                </FormGroup>
 
-//                 {/* <Button size="small">Read</Button>
-//         <Button size="small">Unread</Button> */}
+                {/* <Button size="small">Read</Button>
+        <Button size="small">Unread</Button> */}
 
-//             </CardActions>
-//         </Card>
-//     );
-// }
-
-return (
-    <ImageList cols={3}>
-      
-      {/* {books.map((item) => ( */}
-        <ImageListItem key={books.id}>
-          <img
-            src={`${books.img}`}
-            srcSet={`${books.img}`}
-            alt={books.title}
-            loading="lazy"
-          />
-          <ImageListItemBar
-            title={books.title}
-            subtitle={books.author}
-            actionIcon={
-              <IconButton
-                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                aria-label={`info about ${books.title}`}
-              >
-                <BookmarkAddSharpIcon />
-              </IconButton>
-            }
-          />
-        </ImageListItem>
-      
-      {/* } */}
-    </ImageList>
-  );
+            </CardActions>
+        </Card>
+    );
 }
+
