d100991b5378de9d9a4f7b7a2dddc5355669adad
ReichertA2 <aydee0112@yahoo.com>
2022-08-24 01:45:36 -0500

    added background color to login

--- src/views/Login.js
+++ src/views/Login.js
@@ -8,9 +8,11 @@ export default function Login() {
   const theme = useTheme();
 
   return (
-    <Box sx={{width:500, height:500, backgroundColor:'#3F616D', padding:5, margin:'auto', backgroundImage: "linear-gradient(to top, #3f616d, #3e6a79, #3d7285, #3b7b92, #38849f, #36809a, #347d96, #327991, #30687a, #2e5865, #2b4850, #27383d);", opacity:'0.9'}}>
+  
+    <Box  sx={{width:500, height:500, padding:5, margin:'auto', backgroundColor:'rgba(0,0,0,0.05)', marginTop:"5%"}}>
       <h4 style={{textAlign: 'center', fontSize:'2rem'}}>Login</h4>
       <LoginForm />
     </Box>
+
   );
 }
