8ff8bd28f81a19c11d46cadbd721a9271e4ea871
ReichertA2 <aydee0112@yahoo.com>
2022-08-29 13:55:39 -0500

    added css styling to AllBookBrowser component

--- src/components/AllBookBrowser.js
+++ src/components/AllBookBrowser.js
@@ -100,16 +100,18 @@ export default function AllBookBrowser(filterBy) {
 
   return (
     <div sx={{backgroundImage:"linear-gradient(to top, #28585c, #204e53, #17444b, #0f3a42, #073039, #09313a, #0c333b, #0e343c, #1a4048, #264d54, #315a60, #3d676c);", backgroundSize: "100%"}}>
-    <ImageList cols={3}>
+    <ImageList cols={9}>
       {books.map((item) => (
         <ImageListItem key={item.id}>
-          <img
+          <img style={{height:300, width:200}}
             src={`${item.img}`}
             srcSet={`${item.img}`}
             alt={item.title}
             loading="lazy"
+            
           />
           <ImageListItemBar
+          style={{height:50, }}
             title={item.title}
             subtitle={item.author}
             actionIcon={<>
@@ -118,14 +120,15 @@ export default function AllBookBrowser(filterBy) {
                 aria-label={`info about ${item.title}`}
                 onClick={()=>{handleAddToCart(item)}}
               >
-                <BookmarkAddSharpIcon />
+                <BookmarkAddSharpIcon style={{fontSize:'large',marginTop:'5px'}}/>
               </IconButton>
               <IconButton
-              sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+            
+              sx={{ color: 'rgba(255, 255, 255, 0.54)'}}
               aria-label={`info about ${item.title}`}
               onClick={()=>navigate('/SingleBook/'+item.id)}
             >
-              <InfoIcon />
+              <InfoIcon   style={{fontSize:'large', marginTop:'-12px'}}/>
             </IconButton>
             </> }
           />
